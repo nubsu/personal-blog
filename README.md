e2a7131d50c12bd5e870d5e3d8011e6156224686
mingo <58668454+nubsu@users.noreply.github.com>
2024-05-09 03:41:57 -0500

    added footer

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# personal-blog
+# Personal Blog
+
+This is the repository for my Personal Blog website. Features include:
+
+* Ability to submit and view multiple blog posts utilizing localStorage
+* Dark mode toggle utilizing localStorage
+* A navigation button connecting the form and blog posts
+
+## Screenshot
+
+![A screenshot of the finished first version of my "About Me" wesbite.](./assets/images/Screenshot_9-5-2024_33125_127.0.0.1.jpeg)
+## Link to Github page
+
--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,6 +3,7 @@ for(let i=1;i<=localStorage.count;i++){
         const h2 = document.createElement('h2')
         const h5 = document.createElement('h5')
         const p = document.createElement('p')
+        // const button = document.createElement('button')
         const div = document.createElement('div')
         const start = document.querySelector('#start')
         const blog = start.appendChild(div)
@@ -13,10 +14,15 @@ for(let i=1;i<=localStorage.count;i++){
         p.textContent=shlawgpost.name
     
         blog.appendChild(h2)
+        // blog.appendChild(button)
         blog.appendChild(h5)
         blog.appendChild(p)
     
         blog.setAttribute('class', 'p-4 m-2 border border-secondary border-2 rounded text-break')
+        h2.setAttribute('class', 'd-inline')
+        // button.setAttribute('type', 'button')
+        // button.setAttribute('class', 'btn-close')
+        h5.setAttribute('class', 'pt-3')
         p.setAttribute('class', 'mb-0 mt-3')
     
         console.log(i)
